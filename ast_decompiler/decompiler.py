JelleZijlstra/ast_decompiler

commit a5bd70e3b07e0782e6b44041f80960a9b213d079

fix additional test case
Thanks to Shantanu Jain for finding this one.

ast_decompiler/decompiler.py
@@ -801,7 +801,7 @@ class Decompiler(ast.NodeVisitor):
         self.write(delimiter)
 
     def visit_FormattedValue(self, node):
-        has_parent = isinstance(self.get_parent_node(), ast.JoinedStr)
+        has_parent = isinstance(self.get_parent_node(), (ast.JoinedStr, ast.FormattedValue))
         if not has_parent:
             self.write("f'")
         self.write('{')

tests/test_py3_syntax.py
@@ -154,6 +154,7 @@ def test_FormattedValue():
     check('f"{a.b}c{d()}"')
     check("f'{1/3:.1f}'")
     check(r"f'{a}\''")
+    check("f'{1/3:{5}.1}'")
 
 
 @skip_before((3, 0))

commit 388202ece3490e6d88ad387f0950ecea0ce5b432

f-string changes (#6)
* refactor visit_FormattedValue, visit_JoinedStr

The idea here is that children nodes should be responsible for their own
rendering, even if it is dependent on their parents. This is consistent
with design choices elsewhere in ast_decompiler. The change also means
that self.node_stack reflects the truth, which is what caused the bug in
a5bd70e.

* raise NotImplementedError on nested f-strings

Since there is no plan to fix this in the near future (see issues #5),
rather than fail mysteriously, we should be explicit about our (lack of)
intention.

* fix f-string on {}-like objects

Add space so that we don't accidentally escape curly braces. Additional
test cases added.

ast_decompiler/decompiler.py
@@ -231,6 +231,15 @@ class Decompiler(ast.NodeVisitor):
         else:
             yield
 
+    @contextmanager
+    def f_literalise_if(self, condition):
+        if condition:
+            self.write("f'")
+            yield
+            self.write("'")
+        else:
+            yield
+
     def generic_visit(self, node):
         raise NotImplementedError('missing visit method for %r' % node)
 
@@ -803,37 +812,38 @@ class Decompiler(ast.NodeVisitor):
         self.write(delimiter)
 
     def visit_FormattedValue(self, node):
-        has_parent = isinstance(self.get_parent_node(), (ast.JoinedStr, ast.FormattedValue))
-        if not has_parent:
-            self.write("f'")
-        self.write('{')
-        self.visit(node.value)
-        if node.conversion != -1:
-            self.write('!%s' % chr(node.conversion))
-        if node.format_spec is not None:
-            self.write(':')
-            if isinstance(node.format_spec, ast.JoinedStr):
-                self._visit_joinedstr_body(node.format_spec)
-            elif isinstance(node.format_spec, ast.Str):
-                self.write(node.format_spec.s)
-            else:
-                raise TypeError('format spec must be a string, not {}'.format(node.format_spec))
-        self.write('}')
-        if not has_parent:
-            self.write("'")
+        has_parent = isinstance(self.get_parent_node(), ast.JoinedStr)
+        with self.f_literalise_if(not has_parent):
+            self.write('{')
+            if isinstance(node.value, ast.JoinedStr):
+                raise NotImplementedError('ast_decompiler does not support nested f-strings yet')
+            add_space = isinstance(node.value, (ast.Set, ast.Dict, ast.SetComp, ast.DictComp))
+            if add_space:
+                self.write(' ')
+            self.visit(node.value)
+            if node.conversion != -1:
+                self.write('!%s' % chr(node.conversion))
+            if node.format_spec is not None:
+                self.write(':')
+                if isinstance(node.format_spec, ast.JoinedStr):
+                    self.visit(node.format_spec)
+                elif isinstance(node.format_spec, ast.Str):
+                    self.write(node.format_spec.s)
+                else:
+                    raise TypeError('format spec must be a string, not {}'.format(node.format_spec))
+            if add_space:
+                self.write(' ')
+            self.write('}')
 
     def visit_JoinedStr(self, node):
-        self.write("f'")
-        self._visit_joinedstr_body(node)
-        self.write("'")
-
-    def _visit_joinedstr_body(self, node):
-        for value in node.values:
-            if isinstance(value, ast.Str):
-                # always escape '
-                self.write(value.s.encode('unicode-escape').decode('ascii').replace("'", r"\'"))
-            else:
-                self.visit(value)
+        has_parent = isinstance(self.get_parent_node(), ast.FormattedValue)
+        with self.f_literalise_if(not has_parent):
+            for value in node.values:
+                if isinstance(value, ast.Str):
+                    # always escape '
+                    self.write(value.s.encode('unicode-escape').decode('ascii').replace("'", r"\'"))
+                else:
+                    self.visit(value)
 
     def visit_Bytes(self, node):
         self.write(repr(node.s))

tests/test_py3_syntax.py
@@ -155,6 +155,10 @@ def test_FormattedValue():
     check("f'{1/3:.1f}'")
     check(r"f'{a}\''")
     check("f'{1/3:{5}.1}'")
+    check("f'{ {a, b, c} }'")
+    check("f'{ {a: b} }'")
+    check("f'{ {a for a in b} }'")
+    check("f'{ {a: b for a, b in c} }'")
 
 
 @skip_before((3, 0))